ae9ebbeff43a348ffb37eb8a0929b23772559e7c
Peter Román <peterrom@gmail.com>
2017-11-06 20:02:08 +0100

    First commit of provisioning files

---
+++ Makefile
@@ -0,0 +1,73 @@
+all: general wm other
+
+# general packages and tools
+.apt-repositories:
+	sudo add-apt-repository ppa:ubuntu-elisp/ppa
+	sudo apt-get update
+	touch $@
+
+.apt-packages: .apt-repositories
+	sudo apt-get install -y \
+		git \
+		build-essential \
+		glibc-doc \
+		emacs-snapshot \
+		silversearcher-ag \
+		net-tools \
+		python3.6 \
+		python3.6-venv
+	touch $@
+
+.emacs-config: .apt-packages
+	git clone https://github.com/peterrom/.emacs.d.git ~/.emacs.d
+	touch $@
+
+.PHONY: general
+general: .emacs-config
+
+# dwm as window manager
+dwm: .apt-packages
+	git clone --depth 1 http://git.suckless.org/dwm $@
+
+dmenu: .apt-packages
+	git clone --depth 1 http://git.suckless.org/dmenu $@
+
+.dwm-packages:
+	sudo apt-get install -y \
+		libx11-dev \
+		libxft-dev \
+		libxinerama-dev
+	touch $@
+
+.dwm-install: dwm-config.h .dwm-packages | dwm
+	cp $< dwm/config.h
+	sudo make -C dwm install
+	touch $@
+
+dwmrunner: dwmrunner.c
+	$(CC) -o $@ $< -lX11
+
+/usr/local/bin/dwmrunner: dwmrunner
+	sudo cp $< $@
+
+/usr/share/xsessions/dwm.desktop: dwm.desktop .dwm-install /usr/local/bin/dwmrunner
+	sudo cp $< $@
+
+.dmenu-install: | dmenu
+	sudo make -C dmenu install
+	touch $@
+
+.PHONY: wm
+wm: /usr/share/xsessions/dwm.desktop .dmenu-install
+
+# other settings
+.switch-apple-func-keys-mode:
+	echo options hid_apple fnmode=2 >> /etc/modprobe.d/hid_apple.conf
+	update-initramfs -u -k all
+	touch $@
+
+/etc/xorg.conf: xorg.conf
+	sudo cp $< $@
+
+.PHONY: other
+other: .switch-apple-func-keys-mode /etc/xorg.conf
---
+++ dwmrunner.c
@@ -0,0 +1,112 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <time.h>
+
+#include <X11/Xlib.h>
+
+extern char **environ;
+
+static int
+fileint(const char *filename)
+{
+        int fd;
+        char buf[16];
+        ssize_t sz;
+
+        if ((fd = open(filename, O_RDONLY)) < 0)
+                return -1;
+
+        sz = read(fd, buf, sizeof(buf));
+        (void) close(fd);
+
+        if (sz < 0 || (size_t) sz >= sizeof(buf))
+                return -1;
+
+        buf[sz] = '\0';
+        return atoi(buf);
+}
+
+static const char *
+timestr(void)
+{
+	time_t now;
+	struct tm *ti;
+        static char buf[6];
+
+        if ((now = time(NULL)) < 0 ||
+            (ti = localtime(&now)) == NULL ||
+            snprintf(buf, sizeof(buf), "%02d:%02d", ti->tm_hour, ti->tm_min) < 0)
+                return "--:--";
+
+        return buf;
+}
+
+static const char *
+batstr(void)
+{
+        int now;
+        int full;
+        static char buf[5];
+
+        if ((now = fileint("/sys/class/power_supply/BAT0/charge_now")) < 0 ||
+            (full = fileint("/sys/class/power_supply/BAT0/charge_full")) < 0 ||
+            snprintf(buf, sizeof(buf), "%3d%%", now / (full / 100)) < 0)
+                return "---%";
+
+        return buf;
+}
+
+static const char *
+statusstr(void)
+{
+        static char buf[64];
+        if (snprintf(buf, sizeof(buf), "%s %s", timestr(), batstr()) < 0)
+                return "--";
+
+        return buf;
+}
+
+static void
+status(void)
+{
+	Display *dpy;
+
+	if ((dpy = XOpenDisplay(NULL)) == NULL)
+		abort();
+
+        /* Failing Xlib calls will terminate the application through
+         * the default error handler */
+        (void) XSetWindowBackground(dpy, DefaultRootWindow(dpy), BlackPixel(dpy, DefaultScreen(dpy)));
+        (void) XClearWindow(dpy, DefaultRootWindow(dpy));
+
+	for (;;) {
+		(void) XStoreName(dpy, DefaultRootWindow(dpy), statusstr());
+		(void) XSync(dpy, False);
+
+                /* Ignore early returns */
+		(void) sleep(90);
+	}
+
+	(void) XCloseDisplay(dpy);
+}
+
+static void
+rundwm(void)
+{
+	char *const argv[] = { "/usr/local/bin/dwm", NULL };
+	if (execve(argv[0], argv, environ) < 0)
+                abort();
+}
+
+int
+main(void)
+{
+	if (fork() == 0)
+		status();
+	else
+		rundwm();
+
+	return 0;
+}
